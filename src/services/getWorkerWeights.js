a01b590c94af22b8be768ee161e34069ae56df1c
HandoH <azlilevano@hotmail.com>
2026-03-06 03:07:28 -0300

    tablaSimple_FirebaseActivated

--- src/services/getWorkerWeights.js
+++ src/services/getWorkerWeights.js
@@ -1,14 +1,46 @@
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDoc, getDocs, doc} from "firebase/firestore";
 import { db } from "../../firebase";
 
+const getTodayId = () => {
+  return "2026-03-05"/* new Date().toLocaleDateString("sv-SE", {
+    timeZone: "America/Santiago",
+  });*/
+};
+
 export async function getWorkerWeights(rut) {
+  const weightRef = collection(db, "weightIn");
+  const weightSnap = await getDocs(weightRef);
+  const resultados = [];
+  let totalAmount = 0;
 
-  const snapshot = await getDocs(collection(db, "weightIn"));
+  weightSnap.forEach((docSnap) => {
+    const weights = docSnap.data();
+    const fecha = docSnap.id; // Fechas
+    totalAmount = 0;
 
-  console.log("Snapshot completo:", snapshot);
-  console.log("Docs:", snapshot.docs);
+    if (weights.kilos && Array.isArray(weights.kilos)) {
+      weights.kilos.forEach((item) => {
+        
+        if (item.rut === rut) { // si tienen el mismo rut
+            totalAmount += item.amount
+        }
+      });
+    }
+    if (totalAmount > 0){
+    resultados.push({
+            date: fecha,
+            amount: totalAmount,
+    });
+  }
+  });
 
+  console.log("Resultados filtrados:", resultados);
+  return resultados;
 }
+
+
+
+
 export async function testConnection() {
 
   const snapshot = await getDocs(collection(db, "weightIn"));
